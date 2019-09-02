581f0aa3f061a4c0df3f348b99d987c2d929c93f
Aron Tolentino <arontolentino@outlook.com>
2019-09-01 22:56:40 -0400

    Add header and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div id="app">
     <div class="container">
+      <Header />
       <router-view />
     </div>
   </div>
 </template>
 
+<script>
+import Header from './components/Header';
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+};
+</script>
+
 <style>
+body {
+  padding: 30px
+}
 </style>
