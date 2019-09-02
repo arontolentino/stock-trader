96a7174a48fde469dae7cce7d46501e42654a7c1
Aron Tolentino <arontolentino@outlook.com>
2019-09-01 23:13:01 -0400

    Create Stocks component

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,9 +1,42 @@
 <template>
-  <h1>The Stocks Component</h1>
+  <div>
+    <Stock v-for="stock in stocks" />
+  </div>
 </template>
 
 <script>
+import Stock from './Stock';
+
 export default {
-  name: 'Stocks'
+  name: 'Stocks',
+  data: function() {
+    return {
+      stocks: [
+        {
+          id: 1,
+          name: 'BMW',
+          price: 110
+        },
+        {
+          id: 2,
+          name: 'Google',
+          price: 200
+        },
+        {
+          id: 3,
+          name: 'Apple',
+          price: 250
+        },
+        {
+          id: 4,
+          name: 'Twitter',
+          price: 8
+        }
+      ]
+    };
+  },
+  components: {
+    Stock
+  }
 };
 </script>
